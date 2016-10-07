6536b54f32c0c28d91f7f8fc86653444b2aa03b8
akzi <382018309@qq.com>
2016-10-07 17:41:13 +0800

    add text

--- test/json.cpp
+++ test/json.cpp
@@ -5,118 +5,107 @@
 #include <list>
 #include <vector>
 #include <map>
-#include <fstream>
 #include "acl_cpp/stdlib/string.hpp"
 #include "acl_cpp/stdlib/json.hpp"
-#include "gson_helper.ipp"
-#include "gsoner.h"
 #include <stdio.h>
 #include <iostream>
+#include "struct.h"
+#include "gson.h"
+#include "gsoner.h"
 
-#include "struct2.h"
-#include "gson_gen.h"
-
-void test01()
+void test_list()
 {
-	group_t g1;
-	g1.group_id_ = "10100101";
-	user_t u1,u2,u3;
-	auth_t au1;
-	au1.id = 1;
-	au1.password = new std::string("auth");
-	u1.auth = new std::list<auth_t*>;
-	u1.auth->push_back(new auth_t{ au1});
-	u1.c_str = new char[6]{ "hahah" };
-	u1.id = 100l;
-	u1.username_ = "u1";
-	
-	u2.auth = new std::list<auth_t*>;
-	u2.auth->push_back(new auth_t{ au1 });
-	u2.c_str = new char[6]{ "hahah" };
-	u2.id = 1002;
-	u2.username_ = "u2";
-
-	u3.auth = new std::list<auth_t*>;
-	u3.auth->push_back(new auth_t{ au1 });
-	u3.c_str = new char[6]{ "hahah" };
-	u3.id = 1003;
-	u3.username_ = "u3";
-
-	g1.user_list_.push_back(u1);
-	g1.user_list_.push_back(u2);
-	g1.user_list_.push_back(u3);
-
-	g1.user_ptr_list_.push_back(new user_t{u1});
-	g1.user_ptr_list_.push_back(new user_t{ u2 });
-
-	g1.user_ptr_list_ptr_ = new std::list< user_t*>;
-	g1.user_ptr_list_ptr_->push_back(new user_t{u1});
-	g1.user_ptr_list_ptr_->push_back(new user_t{u2});
-	std::list<user_t> users;
-	users.push_back(u1);
-	users.push_back(u2);
-	users.push_back(u3);
-	g1.user_list_list_.push_back(users);
-	g1.user_list_list_.push_back(users);
-	g1.user = u1;
-
-	g1.vector_user_.push_back(u1);
-	g1.vector_user_.push_back(u2);
-	g1.vector_user_.push_back(u3);
-
-	g1.vector_user_ptr_.push_back(new user_t{ u1 });
-	g1.vector_user_ptr_.push_back(new user_t{ u2 });
-	g1.vector_user_ptr_.push_back(new user_t{ u2 });
-
-	g1.map_user_ptr_.insert(std::make_pair("1",  &u1 ));
-	g1.map_user_ptr_.insert(std::make_pair("2", &u2));
-	g1.map_user_ptr_.insert(std::make_pair("3", &u3));
-
-	g1.int_map_["1"] = 1;
-	g1.int_map_["2"] = 2;
-	g1.int_map_["3"] = 3;
-
-	g1.double_map_["1"] = 0.1;
-	g1.double_map_["2"] = 0.2;
-	g1.double_map_["3"] = 0.3;
-
-	g1.string_map_["1"] = "1";
-	g1.string_map_["2"] = "2";
-	g1.string_map_["3"] = "3";
-
-	g1.int_map_ptr_["1"] = new int (1);
-	g1.int_map_ptr_["2"] = new int(2);
-	g1.int_map_ptr_["3"] = new int(3);
-
-	g1.double_map_ptr_["1"] = new double(0.1);
-	g1.double_map_ptr_["2"] = new double(0.1);
-
-	g1.string_map_ptr_["1"] = new std::string("1");
-	g1.string_map_ptr_["2"] = new std::string("2");
-	g1.string_map_ptr_["3"] = new std::string("3");
+	std::cout << "-----------------test_list--------------------" << std::endl;
+	list_test obj;
+	obj.a.push_back(aab{ 1,2 });
+	obj.a_ptr_ptr = new std::list<aab*>;
+	obj.a_ptr_ptr->push_back(new aab{ 1,2 });
+	obj.a_ptr = new std::list<aab>;
+	obj.a_ptr->push_back(aab{ 1,2 });
+	obj.bools_.push_back(false);
+	obj.bools_ptr = new std::list<bool>;
+	obj.bools_ptr->push_back(false);
 
 	acl::json json;
-	acl::json_node &node = acl::gson::gson(json, g1);
+	acl::json_node &node = acl::gson::gson(json, obj);
+	printf("%s\n", node.to_string().c_str());
 
-	std::cout<<node.to_string().c_str();
-	acl::string str1 = node.to_string();
-	acl::json j2;
-	j2.update(node.to_string().c_str());
-	group_t g2;
-
-	std::pair<bool,std::string> res = 
-		acl::gson::gson(j2.get_root(), g2);
-
-	acl::json j3;
-	acl::json_node &node3 = acl::gson::gson(j3, g2);
-	acl::string str3 = node3.to_string();
-
-	if(str3 == str1)
+	list_test obj2;
+	acl::json json2;
+	json2.update(node.to_string().c_str());
+	std::pair<bool,std::string> ret =  acl::gson::gson(json2.get_root(), obj2);
+	if (ret.first == false)
 	{
-		printf("ok!");
+		printf("%s\n\n", ret.second.c_str());
+		return;
 	}
-	getchar();
+	acl::json json3;
+	acl::json_node &node3 = acl::gson::gson(json3, obj2);
+	printf("%s\n\n", node3.to_string().c_str());
 }
+
+
+void test_vector()
+{
+	std::cout << "-----------------test_vector--------------------" << std::endl;
+	vector_test obj;
+
+	obj.a.push_back(aab{ 1,3 });
+	obj.a_ptr_ptr = new std::vector<aab*>;
+	obj.a_ptr_ptr->push_back(new aab{ 1,2 });
+	obj.a_ptr = new std::vector<aab>;
+	obj.a_ptr->push_back(aab{ 1,2 });
+
+	acl::json json;
+	acl::json_node &node = acl::gson::gson(json, obj);
+	printf("%s\n", node.to_string().c_str());
+
+	vector_test obj2;
+	acl::json json2;
+	json2.update(node.to_string().c_str());
+	std::pair<bool, std::string> ret = acl::gson::gson(json2.get_root(), obj2);
+	if(ret.first == false)
+	{
+		printf("%s\n", ret.second.c_str());
+		return;
+	}
+	acl::json json3;
+	acl::json_node &node3 = acl::gson::gson(json3, obj2);
+	printf("%s\n", node3.to_string().c_str());
+}
+
+void test_map()
+{
+	std::cout << "-----------------test_map--------------------" << std::endl;
+	map_test obj;
+	obj.strig_map.insert({ "1","2" });
+	obj.strig_map_ptr.insert({ "1",new std::string("2") });
+	obj.strig_ptr_map_ptr = new std::map<std::string, std::string*>;
+	obj.strig_ptr_map_ptr->insert({ "1",new std::string("2") });
+
+	obj.aabs.insert({ "1",{1,3} });
+	obj.aabs_ptr.insert({ "1" ,new aab{1,3} });
+	obj.aabs_ptr_ptr = new std::map<std::string, aab*>;
+	obj.aabs_ptr_ptr->insert({ "1" ,new aab{ 1,3 } });
+
+	acl::json json;
+	acl::json_node &node = acl::gson::gson(json, obj);
+	printf("%s\n", node.to_string().c_str());
+	map_test obj2;
+	acl::json json2;
+	json2.update(node.to_string().c_str());
+	std::pair<bool, std::string> ret = acl::gson::gson(json2.get_root(), obj2);
+	if(ret.first == false)
+	{
+		printf("%s\n", ret.second.c_str());
+		return;
+	}
+	acl::json json3;
+	acl::json_node &node3 = acl::gson::gson(json3, obj2);
+	printf("%s\n", node3.to_string().c_str());
+}
+
+
 void test02()
 {
 	acl::gson::gsoner gr;
@@ -124,51 +113,19 @@ void test02()
 	gr.parse_code();
 }
 
-
-void test03()
+void test04()
 {
-	acl::json json;
-	user_t user{ "haha",  int(1) };
-	user.auth = new std::list<auth_t*>;
-	user.auth->push_back(new auth_t{ 111, new std::string("aaaa") });
-	user.c_str = "hello world";
-	acl::json_node &node = acl::gson::gson(json, user);
+	acl::gson::gsoner gr;
 
-
-
-	acl::json j2;
-	j2.update(node.to_string().c_str());
-	j2.get_root();
-	user_t u;
-	if(acl::gson::gson(j2.get_root(), u).first)
-	{
-		acl::json jj;
-		printf(acl::gson::gson(jj, u).to_string().c_str());
-	}
+	gr.read_multi_file({"struct.h" });
+	gr.parse_code();
+	gr.gen_gson();
 }
-#define test_1() printf("1");
-#define test_2(a,b) printf("2");
-#define test_3(a,b,c) printf("3");
-#define test_4(a,b,c,d) printf("4");
 
 int main(void)
 {
-	typedef const user_t const_user_t;
-	//std::cout << std::is_floating_point<std::remove_pointer<const float*>::type>::value;
-	//std::cout << acl::gson::is_string_type<const std::string*>::value;
-	//std::cout << std::is_class</*typename std::remove_pointer<*/const std::string/*>::type*/>::value;
-	std::cout << std::is_object<int>::value;
-	getchar();
-	test01();
-	//test03();
-	//test02();
-	return 0;
-	std::string str = "std::string";
-	std::string sub = str.substr(str.find("::"));
-	printf(sub.c_str());
-	getchar();
-	test01();
-	return 0;
-	printf("enter any key to exit\r\n");
+	test_vector();
+	test_list();
+	test_map();
 	return 0;
 }
---
+++ test/struct.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <string>
+#include <list>
+#include <map>
+#include <vector>
+
+
+struct aab 
+{
+	int a;
+	int b;
+};
+struct list_test
+{
+	std::list<aab> a;
+ 	std::list<aab> *a_ptr;
+	std::list<aab*> *a_ptr_ptr;
+
+	std::list<bool> bools_;
+	std::list<bool> *bools_ptr;
+};
+
+struct vector_test
+{
+	std::vector<aab> a;
+	std::vector<aab> *a_ptr;
+	std::vector<aab*> *a_ptr_ptr;
+};
+
+struct map_test
+{
+	std::map<std::string, std::string> strig_map;
+	std::map<std::string, std::string*> strig_map_ptr;
+	std::map<std::string, std::string*> *strig_ptr_map_ptr;
+	std::map<std::string, aab> aabs;
+	std::map<std::string, aab*> aabs_ptr;
+	std::map<std::string, aab*> *aabs_ptr_ptr;
+};
+
+
+
